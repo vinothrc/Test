6daaca3fbb75db5916439b03ed731e6742af52c8
Vinoth Rc <20122644+vinothrc@users.noreply.github.com>
2020-02-28 18:26:50 +0530

    commit 1

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $msg = "First line of text\nSecond line of text\n\nThanks";
 // use wordwrap() if lines are longer than 70 characters
 $msg = wordwrap($msg,70);
 
-$headers = "From: test1@test.com" . "\r\n";
+$headers = "From: support@webapplicationsite.com" . "\r\n";
 // send email
 mail("test-sdffe@mail-tester.com","This is the Mail Subject",$msg, $headers);
 ?>
